003ec559e201a57aecc9d0d43d784fa2f8fbf04f
Nameless1004 <jjho1029@gmail.com>
2024-10-16 04:58:48 +0900

    docker testz

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/get")
     public String getTest() {
-        return "tezzzzzzzzzzzza3zzzz3zz2t";
+        return "tezzzzzzzzzzzzzza3zzzz3zz2t";
     }
 }
